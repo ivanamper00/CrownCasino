093b00a189cd5a5214056bd128a948446c6c1751
Ivan Amper <ivanamper00@github.com>
2022-02-23 16:37:46 +0800

    first commit

---
+++ app/src/main/java/com/dakuinternational/crown_casino/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dakuinternational.crown_casino
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
